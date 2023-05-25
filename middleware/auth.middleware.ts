f0c19d2aca6c2ee835e16f11deac981ee9f3756e
hossainchisty <hossain.chisty11@gmail.com>
2023-05-25 17:17:48 +0600

    refactor: Update auth middleware to retrieve user info from the database
    This commit updates the auth middleware to retrieve user information based on the decoded user ID from the token. The middleware now calls the user model's findById method to get user data from the database, adds the user object to the request, and passes it on to the next middleware or route handler. This improves the security of the application by ensuring that the user information is always up to date and is retrieved securely.

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,27 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel";
 
 interface AuthenticatedRequest extends Request {
   user?: any;
 }
 
-const protect = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-  const token = req.header('Authorization')?.split(' ')[1];
+const protect = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Access denied' });
+    return res.status(401).json({ message: "Access denied" });
   }
 
   try {
-    const decoded = jwt.verify(token, 'your-secret-key');
+    // Verify and decode the token
+    const decoded = jwt.verify(token, "your-secret-key") as { userId: string };
     req.user = decoded;
+    // Retrieve user information from the database based on the decoded user ID
+    const userId = decoded.userId;
+    const user = User.findById(userId);
+
+    // Add the user object to the request for further processing
+    req.user = user;
+    // Call the next middleware or route handler
     next();
   } catch (error) {
     console.error(error);
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
 export default protect;
-
-
