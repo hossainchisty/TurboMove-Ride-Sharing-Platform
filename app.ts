5d99cb2f8f5c67df676245970f3780f6fed7c11c
hossainchisty <hossain.chisty11@gmail.com>
2023-05-25 18:59:28 +0600

    feat: Add Rider API implementation
    This commit adds API implementation of the Rider resource by importing riderRouters in the app.js file. Now, '/api/v1/rider' routes can be used to interact with the Rider resource.

--- app.ts
+++ app.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from "express";
 import connectDB from "./config/_db";
 connectDB();
 
-
-
-import authRouters from "./routers/auth.routes";
+import authRouters from "./routers/auth.routers";
+import riderRouters from "./routers/rider.routers";
 
 const app = express();
 
@@ -12,6 +11,7 @@ app.use(express.json()); // Parsing JSON bodies
 
 // Rotuers implementation
 app.use("/api/v1/auth", authRouters);
+app.use("/api/v1/rider", riderRouters);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
