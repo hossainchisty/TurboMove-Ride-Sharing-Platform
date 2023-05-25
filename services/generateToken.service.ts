a9b0189a83baca4326a719ce35fecb3e10954a87
hossainchisty <hossain.chisty11@gmail.com>
2023-05-25 16:51:20 +0600

    refactor: Update token expiration to 7 days from 1 hour
    The expiration time for tokens generated by the generateToken service was updated from 1 hour to 7 days to improve user experience and reduce the frequency of repeated logins.

--- services/generateToken.service.ts
+++ services/generateToken.service.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 // Generate JWT token
 const generateToken = (userId: string): string => {
-  return jwt.sign({ userId }, "your-secret-key", { expiresIn: "1h" });
+  return jwt.sign({ userId }, "your-secret-key", { expiresIn: "7d" });
 };
 
 export default generateToken;
